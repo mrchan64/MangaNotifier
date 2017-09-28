e99dd20ed5760184c38275eca90560ff614a0e93
mrchan <wildcatrobotics2011@gmail.com>
2017-09-28 00:02:09 -0700

    working on retrieval

--- index.ps1
+++ index.ps1
@@ -3,5 +3,6 @@ Import-Module (Resolve-Path lib\WebtoonRetrievalFunctions.psm1) -force
 
 ReadMetaData
 $fone = (PrintMetaData)[0]
-RetrieveChapterImgs -Url "$($fone.site)$($fone.urltitle)$($fone.suffix)" -Chap 330 -Name $fone.name
+#$path = (RetrieveChapterImgs -Url "$($fone.site)$($fone.urltitle)$($fone.suffix)" -Chap 130 -Name $fone.name).path
+RetrieveChapterUrl -Url "$($fone.site)$($fone.urltitle)$($fone.suffix)" -Chap 130
 #WriteMetaData
--- lib/WebtoonRetrievalFunctions.psm1
+++ lib/WebtoonRetrievalFunctions.psm1
@@ -1,12 +1,28 @@
 [string]$BaseSite = "http://www.webtoons.com/"
 
-function RetrieveChapterUrl
+function RetrieveChapterUrlOnPage
 {
     param(
         [int]$Chap,
         [string]$Url
     )
     $epUrl = (((Invoke-WebRequest -URI $Url).content | Select-String "http://.{0,}episode_no=$Chap" -AllMatches).Matches | Select-Object -First 1).value
+    $epUrl
+}
+
+function RetrieveChapterUrl
+{
+    param(
+        [int]$Chap,
+        [string]$Url
+    )
+    $counter = 1
+    while([string]::IsNullOrEmpty($epUrl) -Or [string]::IsNullOrEmpty($isch1)){
+        Write-Host "$Url&page=$counter"
+        $epUrl = RetrieveChapterUrlOnPage -Url "$Url&page=$counter" -Chap $Chap
+        $isch1 = RetrieveChapterUrlOnPage -Url "$Url&page=$counter" -Chap 1
+        $counter = $counter + 1
+    }
     $epUrl
 }
 
@@ -19,6 +35,10 @@ function RetrieveChapterImgs
     )
 
     $epUrl = RetrieveChapterUrl -Chap $Chap -Url $Url
+    if([string]::IsNullOrEmpty($epUrl)){
+        Write-Host "$Name Chapter $Chap does not exist."
+        return
+    }
     $webData = (Invoke-WebRequest -URI $epUrl).content
     $images = Select-String "<img src=""http://webtoons.static.naver.net/image/bg_transparency.png"" width=""[0-9,.]{0,}"" height=""[0-9,.]{0,}"" alt=""image"" class=""_images"" data-url=""http://webtoon.phinf.naver.net.{0,}"" rel=""nofollow"" ondragstart=""return false;"" onselectstart=""return false;"" oncontextmenu=""return false;"">" -input $webData -AllMatches
 
@@ -43,5 +63,6 @@ function RetrieveChapterImgs
         Write-Progress -Activity "Downloaded " -Status "$counter of $total images"
         $counter = $counter+1
         [io.file]::WriteAllBytes($tempImgPath, $imgData)
-}
+    }
+    return $tempdir
 }
